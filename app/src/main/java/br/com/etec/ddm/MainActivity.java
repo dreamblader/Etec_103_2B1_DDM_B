0090c200b3fed68ee36f102f72d0f84551c26355
Carlos <carlosgabrielmaster@gmail.com>
2021-03-17 08:57:29 -0300

    aula-17-03

--- app/src/main/java/br/com/etec/ddm/MainActivity.java
+++ app/src/main/java/br/com/etec/ddm/MainActivity.java
@@ -1,8 +1,12 @@
 package br.com.etec.ddm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatButton novoHorario = findViewById(R.id.am_criar_horas_btn);
+        novoHorario.setOnClickListener(view -> {
+            view.setBackgroundColor(getColor(R.color.black));
+            Toast.makeText(this, "Voce clicou aqui!", Toast.LENGTH_LONG).show();
+        });
     }
 }
