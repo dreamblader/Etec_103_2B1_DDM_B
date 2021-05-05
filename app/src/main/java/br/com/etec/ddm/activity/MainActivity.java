09ef1f89b40de30bc4b701324159b110c7a4f3bf
Carlos <carlosgabrielmaster@gmail.com>
2021-05-05 08:46:01 -0300

    aula-05-05

--- app/src/main/java/br/com/etec/ddm/activity/MainActivity.java
+++ app/src/main/java/br/com/etec/ddm/activity/MainActivity.java
@@ -6,51 +6,101 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.Adapter;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 import br.com.etec.ddm.R;
 import br.com.etec.ddm.adapter.ScheduleDetailAdapter;
 import br.com.etec.ddm.dialog.NewScheduleDialog;
 import br.com.etec.ddm.model.ScheduleModel;
 
-import static java.util.Collections.emptyList;
+import static br.com.etec.ddm.util.DateConverter.dayOfWeek;
+import static br.com.etec.ddm.util.DateConverter.monthName;
 
 public class MainActivity extends AppCompatActivity implements NewScheduleDialog.SaveCallback {
 
-    List<ScheduleModel> scheduleList = new ArrayList<>();
-    ScheduleDetailAdapter adapter;
+    List<ScheduleModel> morningScheduleList = new ArrayList<>();
+    ScheduleDetailAdapter morningAdapter;
+
+    List<ScheduleModel> afternoonScheduleList = new ArrayList<>();
+    ScheduleDetailAdapter afternoonAdapter;
+
+    List<ScheduleModel> nightScheduleList = new ArrayList<>();
+    ScheduleDetailAdapter nightAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupView();
         setupRecyclerView();
+        setupListeners();
+    }
+
+    private void setupView(){
+        Calendar calendar = Calendar.getInstance();
+        String weekDay = dayOfWeek(calendar.get(Calendar.DAY_OF_WEEK));
+        String monthName = monthName(calendar.get(Calendar.MONTH));
+        String titleString = weekDay+" - "+calendar.get(Calendar.DAY_OF_MONTH)+" de "+monthName;
+
+        TextView title = findViewById(R.id.ids_day_name);
+        title.setText(titleString);
+    }
+
+    private void setupRecyclerView(){
+        morningAdapter = new ScheduleDetailAdapter(morningScheduleList);
+        afternoonAdapter = new ScheduleDetailAdapter(afternoonScheduleList);
+        nightAdapter = new ScheduleDetailAdapter(nightScheduleList);
+
+        RecyclerView morningView = findViewById(R.id.ids_morning_list);
+        morningView.setLayoutManager(new LinearLayoutManager(this));
+        morningView.setAdapter(morningAdapter);
+
+        RecyclerView afternoonView = findViewById(R.id.ids_afternoon_list);
+        afternoonView.setLayoutManager(new LinearLayoutManager(this));
+        afternoonView.setAdapter(afternoonAdapter);
+
+        RecyclerView nightView = findViewById(R.id.ids_night_list);
+        nightView.setLayoutManager(new LinearLayoutManager(this));
+        nightView.setAdapter(nightAdapter);
+    }
+
+    private void setupListeners(){
         AppCompatButton novoHorario = findViewById(R.id.am_criar_horas_btn);
         novoHorario.setOnClickListener(view -> {
             NewScheduleDialog dialog = new NewScheduleDialog();
             dialog.show(getSupportFragmentManager(), NewScheduleDialog.class.getName());
         });
-
-    }
-
-    private void setupRecyclerView(){
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-
-        adapter = new ScheduleDetailAdapter(scheduleList);
-
-        RecyclerView morningView = findViewById(R.id.ids_morning_list);
-        morningView.setLayoutManager(linearLayoutManager);
-        morningView.setAdapter(adapter);
     }
 
     @Override
     public void saveScheduleModel(ScheduleModel model) {
-        scheduleList.add(model);
+        String initHour = model.getInitialHour();
+
+        if(initHour.compareTo("12:00") <= 0){
+
+            morningScheduleList.add(model);
+            morningAdapter.notifyDataSetChanged();
+
+        } else if(initHour.compareTo("19:00")<= 0){
+
+            afternoonScheduleList.add(model);
+            afternoonAdapter.notifyDataSetChanged();
+
+        } else {
+
+            nightScheduleList.add(model);
+            nightAdapter.notifyDataSetChanged();
+
+        }
+
         System.out.println(model.toString());
     }
+
+
 }
---
+++ app/src/main/java/br/com/etec/ddm/util/DateConverter.java
@@ -0,0 +1,58 @@
+package br.com.etec.ddm.util;
+
+public class DateConverter {
+
+    static public String dayOfWeek(int day) {
+
+        switch (day) {
+            case 1:
+                return "Domingo";
+            case 2:
+                return "Segunda";
+            case 3:
+                return "Terça";
+            case 4:
+                return "Quarta";
+            case 5:
+                return "Quinta";
+            case 6:
+                return "Sexta";
+            case 7:
+                return "Sabado";
+            default:
+                return "";
+        }
+    }
+
+    static public String monthName(int month){
+
+        switch (month){
+            case 0:
+                return "Janeiro";
+            case 1:
+                return "Fevereiro";
+            case 2:
+                return "Março";
+            case 3:
+                return "Abril";
+            case 4:
+                return "Maio";
+            case 5:
+                return "Junho";
+            case 6:
+                return "Julho";
+            case 7:
+                return "Agosto";
+            case 8:
+                return "Setembro";
+            case 9:
+                return "Outubro";
+            case 10:
+                return "Novembro";
+            case 11:
+                return "Dezembro";
+            default:
+                return "";
+        }
+    }
+}
